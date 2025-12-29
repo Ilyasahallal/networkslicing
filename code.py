02bc359ea21c6bb736977dc087647cfd58861119
Ilyasahallal <159455114+Ilyasahallal@users.noreply.github.com>
2025-12-29 11:58:22 +0100

    Refactor network topology setup and controller initialization

--- code.py
+++ code.py
@@ -14,18 +14,14 @@ class FiveGCoreTopology:
             autoSetMacs=True
         )
 
-        # SDN Controller
-        self.sdn_controller = self.net.addController(
-            name='sdn_ctrl',
+        self.ctrl = self.net.addController(
+            'c0',
             controller=RemoteController,
             ip='127.0.0.1',
             port=6633
         )
 
     def create_topology(self):
-        """Create the complete 5G core topology"""
-
-        # ---------------- Core 5G Functions ----------------
         info('*** Adding 5G Core Functions\n')
 
         self.amf  = self.net.addSwitch('amf',  dpid='0000000000000101')
@@ -36,117 +32,72 @@ class FiveGCoreTopology:
         self.pcf  = self.net.addSwitch('pcf',  dpid='0000000000000106')
         self.ausf = self.net.addSwitch('ausf', dpid='0000000000000107')
 
-        # ---------------- gNBs ----------------
         info('*** Adding gNBs\n')
         self.gnbs = []
-
         for i in range(4):
             dpid = f'{0x200 + i:016x}'
             gnb = self.net.addSwitch(f'gnb{i+1}', dpid=dpid)
             self.gnbs.append(gnb)
 
-        # ---------------- UEs ----------------
         info('*** Adding UEs\n')
         self.ues = []
-
         for i in range(8):
             ue = self.net.addHost(
-                name=f'ue{i+1}',
-                ip=f'10.0.{i+1}.{i+1}/24',
-                mac=f'00:00:00:00:0{i+1}:0{i+1}'
+                f'ue{i+1}',
+                ip=f'10.0.{i+1}.{i+1}/24'
             )
             self.ues.append(ue)
 
-        # ---------------- Cloud / Internet ----------------
-        info('*** Adding cloud servers\n')
+        info('*** Adding servers\n')
+        self.mec = self.net.addHost('mec', ip='10.100.1.1/24')
+        self.inet = self.net.addHost('inet', ip='10.200.1.1/24')
 
-        self.mec_server = self.net.addHost(
-            'mec_server',
-            ip='10.100.1.1/24',
-            mac='00:00:00:00:fe:01'
-        )
+        info('*** Creating links\n')
 
-        self.internet_gw = self.net.addHost(
-            'internet_gw',
-            ip='10.200.1.1/24',
-            mac='00:00:00:00:ff:01'
-        )
-
-        # ---------------- Links with QoS ----------------
-        info('*** Creating links with QoS\n')
-
-        # Core network links (high capacity, low latency)
+        # Core
         self.net.addLink(self.amf, self.smf,  bw=100, delay='1ms')
         self.net.addLink(self.amf, self.upf1, bw=100, delay='1ms')
         self.net.addLink(self.amf, self.upf2, bw=100, delay='1ms')
         self.net.addLink(self.smf, self.upf1, bw=100, delay='1ms')
         self.net.addLink(self.smf, self.upf2, bw=100, delay='1ms')
-        self.net.addLink(self.amf, self.udm,  bw=10,  delay='2ms')
-        self.net.addLink(self.amf, self.pcf,  bw=10,  delay='2ms')
-        self.net.addLink(self.amf, self.ausf, bw=10,  delay='2ms')
+        self.net.addLink(self.amf, self.udm,  bw=10, delay='2ms')
+        self.net.addLink(self.amf, self.pcf,  bw=10, delay='2ms')
+        self.net.addLink(self.amf, self.ausf, bw=10, delay='2ms')
 
         # gNB → AMF
         for i, gnb in enumerate(self.gnbs):
-            self.net.addLink(
-                gnb,
-                self.amf,
-                bw=10,
-                delay=f'{5 + i}ms'
-            )
+            self.net.addLink(gnb, self.amf, bw=10, delay=f'{5+i}ms')
 
-        # UE → gNB (radio access)
+        # UE → gNB
         for i, ue in enumerate(self.ues):
-            gnb_index = i % len(self.gnbs)
-            self.net.addLink(
-                ue,
-                self.gnbs[gnb_index],
-                bw=1,
-                delay=f'{10 + gnb_index}ms'
-            )
+            gnb = self.gnbs[i % len(self.gnbs)]
+            self.net.addLink(ue, gnb, bw=1, delay='10ms')
 
-        # UPF → Cloud
-        self.net.addLink(self.upf1, self.mec_server, bw=10, delay='2ms')
-        self.net.addLink(self.upf2, self.internet_gw, bw=10, delay='5ms')
+        # UPF → servers
+        self.net.addLink(self.upf1, self.mec,  bw=10, delay='2ms')
+        self.net.addLink(self.upf2, self.inet, bw=10, delay='5ms')
 
-    def start_network(self):
-        """Start the network"""
+    def start(self):
         info('*** Starting network\n')
-
         self.net.build()
-        self.sdn_controller.start()
+        self.ctrl.start()
 
         for sw in self.net.switches:
-            sw.start([self.sdn_controller])
+            sw.start([self.ctrl])
 
         info('*** Testing connectivity\n')
         self.net.pingAll()
 
-    def run_performance_tests(self):
-        """Run basic 5G performance tests"""
-        info('*** Running performance tests\n')
-
-        info('*** UE1 → MEC bandwidth test\n')
-        self.ues[0].cmd(
-            'iperf3 -c 10.100.1.1 -t 10 > ue1_mec.txt &'
-        )
-
-        info('*** Latency tests\n')
-        for i, ue in enumerate(self.ues[:3]):
-            result = ue.cmd('ping -c 5 10.100.1.1')
-            info(f'Latency UE{i+1} → MEC:\n{result}\n')
-
     def run(self):
         self.create_topology()
-        self.start_network()
-        self.run_performance_tests()
+        self.start()
         CLI(self.net)
         self.net.stop()
 
 
 def main():
     setLogLevel('info')
-    topology = FiveGCoreTopology()
-    topology.run()
+    FiveGCoreTopology().run()
 
 
 if __name__ == '__main__':
